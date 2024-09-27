b7290fe7a90abadd73597ad6ae09a13391774420
zalivka <20828522+zalivka@users.noreply.github.com>
2024-09-27 18:48:26 +0300

    dee

--- img.py
+++ img.py
@@ -2,7 +2,7 @@ from flask import Flask, send_file, current_app
 
 app = Flask(__name__)
 
-@app.route('/logo')
+@app.route('/')
 def flask_logo():
     return current_app.send_static_file('mask_0.png')
 
