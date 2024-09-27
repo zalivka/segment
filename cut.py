5b899e0c7b5332181baccce355def228003123fc
zalivka <20828522+zalivka@users.noreply.github.com>
2024-09-27 17:50:40 +0300

    dee

--- cut.py
+++ cut.py
@@ -98,8 +98,10 @@ image = np.array(image.convert("RGB"))
 from sam2.build_sam import build_sam2
 from sam2.sam2_image_predictor import SAM2ImagePredictor
 
-sam2_checkpoint = "checkpoints/sam2_hiera_large.pt"
-model_cfg = "sam2_hiera_l.yaml"
+# sam2_checkpoint = "checkpoints/sam2_hiera_large.pt"
+# model_cfg = "sam2_hiera_l.yaml"
+sam2_checkpoint = "../segment-anything-2/checkpoints/sam2_hiera_large.pt"
+model_cfg = "../segment-anything-2/sam2_hiera_l.yaml"
 
 sam2_model = build_sam2(model_cfg, sam2_checkpoint, device=device)
 
